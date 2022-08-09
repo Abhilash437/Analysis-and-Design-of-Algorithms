33163b90bb049404755eaab35039193c7fb45727
Abhilash S Hathwar <76783449+Abhilash437@users.noreply.github.com>
2022-08-09 16:52:26 +0530

    Final commit

--- DFS/source.c
+++ DFS/source.c
@@ -1,75 +1,66 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define initial 1
+#define visited 2
+#define finished 3
+
+#define MAX 100
+
+int state[MAX];
+int V[MAX];
 int count;
-int bop;
-int cyclic;
-int V[100];
+int cycles;
 
-void cycles(int v,int *a[100],int n){
-	for(int i = 0;i<n;i++){
-		if(a[v][i] == 1 && V[i]!=0 && V[v] - V[i]!=1){
-			cyclic++;
-			printf("Backedge from %d to %d\n",v,i);
-		}
-	}
-}
-void dfs(int v,int *a[100],int n){
-
+void DFS(int a[100][100],int v,int n){
+	int i;
+	state[v] = visited;
 	count++;
 	V[v] = count;
-	
-	printf("%d\n",v);
-	//cycles(v,a,n);
-	// for(int i=0;i<n;i++){
-	// 	if(a[v][i] == 1 && V[i]!=0 && V[v]-V[i]!=1){
-	// 		cyclic++;
-	// 		printf("Backedge from %d to %d\n",v,i);
-	// 	}
-	// }
-	for(int i = 0;i<n;i++){
-		bop++;
+	printf("%d - %d\n",v,count);
+	for(i = 0;i<n;i++){
 		if(a[v][i] == 1){
-			bop++;
-			if(V[i] == 0)
-				dfs(i,a,n);
-			else if(V[v]>V[i] || ){
-				cyclic++;
+			if(state[i] == initial)
+				DFS(a,i,n);
+			else if(state[i] == finished){
+				printf("There exists a back edge from %d to %d\n",v,i);
+				cycles++;
 			}
 		}
 	}
+	state[v] = finished;
 
 }
 
-void DFS(int *a[100],int n){
-	count = 0;
-	for(int i = 0;i<n;i++)
+void dfs(int a[100][100],int n){
+	for(int i = 0;i<n;i++){
 		V[i] = 0;
-	bop = 0;
+		state[i] = initial;
+	}
 	count = 0;
-	cyclic = 0;
-	int connected = -1;
-	for(int i = 0;i<n;i++)
-		if(V[i] == 0){
-			printf("DFS tree: %d\n",connected+2);
-			dfs(i,a,n);
-			connected++;
-		}
-	if(cyclic>0)
-		printf("Graph has cycles\n");
-}
+	cycles = 0;
+	//DFS(a,0,n);
 
+	for(int i = 0;i<n;i++){
+		if(state[i] == initial){
+			//printf("%d\n",i);
+			DFS(a,i,n);
+		}
+	}
+	if(cycles>0)
+		printf("There exists cycles in the graph\n");
+	else
+		printf("Graph is acyclic\n");
+}
 
 int main(){
-	int v;
-	printf("Enter the number of vertices in the graph\n");
-		scanf("%d",&v);
-	int *a[v];
-	for(int i = 0;i<v;i++)
-		a[i] = (int *)malloc(sizeof(int)*v);
+	int n;
+	printf("Enter the number of vertices\n");
+	scanf("%d",&n);
+	int a[100][100];
 	printf("Enter the adjacency matrix\n");
-	for(int i = 0;i<v;i++)
-		for(int j = 0;j<v;j++)
+	for(int i = 0;i<n;i++)
+		for(int j = 0;j<n;j++)
 			scanf("%d",&a[i][j]);
-	DFS(a,v);
+	dfs(a,n);
 }
